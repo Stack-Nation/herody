26f3fafaf8b5b2168e4f4b47e064845cd1797e27
Lokendra Soni <lokendrasoni10@gmail.com>
2021-03-02 09:58:14 +0530

    Added backend

---
+++ database/migrations/2020_01_21_140013_create_projects_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('des');
+            $table->string('cat');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('duration');
+            $table->string('stipend');
+            $table->longText('benefits');
+            $table->string('place');
+            $table->string('count');
+            $table->longText('skills');
+            $table->string('proofs');
+            $table->bigInteger('user');
+            $table->integer('mobile')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
